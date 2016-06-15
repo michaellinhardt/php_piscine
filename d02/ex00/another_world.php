03cc8234824dc3d6b59a39e6c990f3799c378729
Michael LINHARDT <nestoyeur@gmail.com>
2016-06-15 02:46:04 +0200

    d02 ex01

--- d02/ex00/another_world.php
+++ d02/ex00/another_world.php
@@ -1,2 +1,2 @@
 #!/usr/bin/php
-<?php if ($argc > 1) echo  preg_replace('/\s+/', ' ', trim($argv[1])) ."\n"; ?>
+<?php if ($argc > 1) echo preg_replace('/\s+/', ' ', trim($argv[1])) . "\n"; ?>
--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -1,70 +1,65 @@
 #!/usr/bin/php
 <?php
-function syntax_error()
+function exit1($sMsg = NULL)
 {
-	echo "Wrong Format\n";
+	if ($sMsg)
+		echo $sMsg . "\n";
 	exit(1);
 }
-if ($argc < 2)
-	exit(1);
-else if ($argc > 2)
-	syntax_error();
-$aDate = explode(" ", $argv[1]);
-$aDate = array_filter($aDate);
-$aDate = array_slice($aDate, 0);
-if (count($aDate) != 5)
-    syntax_error();
-$sDate = implode(" ", $aDate);
-if (preg_match("/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/", $sDate) != 1)
-    syntax_error();
-$sDateOld = $sDate;
-$sDate = preg_replace("/[Jj]anvier/", '01', $sDate);
-$sDate = preg_replace('/[Ff]evrier/', '02', $sDate);
-$sDate = preg_replace('/[Mm]ars/', '02', $sDate);
-$sDate = preg_replace('/[Aa]vril/', '03', $sDate);
-$sDate = preg_replace('/[Mm]ai/', '05', $sDate);
-$sDate = preg_replace('/[Jj]uin/', '06', $sDate);
-$sDate = preg_replace('/[Jj]uillet/', '07', $sDate);
-$sDate = preg_replace('/[Aa]out/', '08', $sDate);
-$sDate = preg_replace('/[Ss]eptembre/', '09', $sDate);
-$sDate = preg_replace('/[Oo]ctobre/', '10', $sDate);
-$sDate = preg_replace('/[Nn]ovembre/', '11', $sDate);
-$sDate = preg_replace('/[Dd]ecembre/', '12', $sDate);
-if ($sDateOld === $sDate)
-    syntax_error();
-$sDateOld = $sDate;
-$sDate = preg_replace('/[Dd]imanche/', '0', $sDate);
-$sDate = preg_replace('/[Ll]undi/', '1', $sDate);
-$sDate = preg_replace('/[Mm]ardi/', '2', $sDate);
-$sDate = preg_replace('/[Mm]ercredi/', '3', $sDate);
-$sDate = preg_replace('/[Jj]eudi/', '4', $sDate);
-$sDate = preg_replace('/[Vv]endredi/', '5', $sDate);
-$sDate = preg_replace('/[Ss]amedi/', '6', $sDate);
-if ($sDateOld === $sDate)
-    syntax_error();
-$trying_to_parse = strptime($sDate, "%w %d %m %Y %H:%M:%S");
-if ($trying_to_parse === FALSE)
-    syntax_error();
-if (strlen($trying_to_parse["unparsed"]) > 0)
-    syntax_error();
-unset($filtered[0]);
-$original = implode(" ", $filtered);
-$original = preg_replace("/[Jj]anvier/", 'January', $original);
-$original = preg_replace('/[Ff]evrier/', 'February', $original);
-$original = preg_replace('/[Mm]ars/', 'March', $original);
-$original = preg_replace('/[Aa]vril/', 'April', $original);
-$original = preg_replace('/[Mm]ai/', 'May', $original);
-$original = preg_replace('/[Jj]uin/', 'June', $original);
-$original = preg_replace('/[Jj]uillet/', 'July', $original);
-$original = preg_replace('/[Aa]out/', 'August', $original);
-$original = preg_replace('/[Ss]eptembre/', 'September', $original);
-$original = preg_replace('/[Oo]ctobre/', 'October', $original);
-$original = preg_replace('/[Nn]ovembre/', 'November', $original);
-$original = preg_replace('/[Dd]ecembre/', 'December', $original);
+if (!($sArg = trim($argv[1])))
+	exit1();
+else if (preg_match("/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/", $argv[1]) != 1)
+	exit1("Wrong Format");
+else if (count($aArg = explode(" ", $sArg)) != 5)
+	exit1("Wrong Format");
+$sVerif = $sArg;
+$sArg = preg_replace("/[Jj]anvier/", '01', $sArg);
+$sArg = preg_replace('/[Ff]evrier/', '02', $sArg);
+$sArg = preg_replace('/[Mm]ars/', '02', $sArg);
+$sArg = preg_replace('/[Aa]vril/', '03', $sArg);
+$sArg = preg_replace('/[Mm]ai/', '05', $sArg);
+$sArg = preg_replace('/[Jj]uin/', '06', $sArg);
+$sArg = preg_replace('/[Jj]uillet/', '07', $sArg);
+$sArg = preg_replace('/[Aa]out/', '08', $sArg);
+$sArg = preg_replace('/[Ss]eptembre/', '09', $sArg);
+$sArg = preg_replace('/[Oo]ctobre/', '10', $sArg);
+$sArg = preg_replace('/[Nn]ovembre/', '11', $sArg);
+$sArg = preg_replace('/[Dd]ecembre/', '12', $sArg);
+if ($sVerif === $sArg)
+	exit1("Wrong Format");
+$sVerif = $sArg;
+$sArg = preg_replace('/[Dd]imanche/', '0', $sArg);
+$sArg = preg_replace('/[Ll]undi/', '1', $sArg);
+$sArg = preg_replace('/[Mm]ardi/', '2', $sArg);
+$sArg = preg_replace('/[Mm]ercredi/', '3', $sArg);
+$sArg = preg_replace('/[Jj]eudi/', '4', $sArg);
+$sArg = preg_replace('/[Vv]endredi/', '5', $sArg);
+$sArg = preg_replace('/[Ss]amedi/', '6', $sArg);
+if ($sVerif === $sArg)
+	exit1("Wrong Format");
+$aParse = strptime($sArg, "%w %d %m %Y %H:%M:%S");
+if (!$aParse)
+	exit1("Wrong Format");
+else if (strlen($aParse["unparsed"]) > 0)
+	exit1("Wrong Format");
+unset($aArg[0]);
+$sArg = implode(" ", $aArg);
+$sArg = preg_replace("/[Jj]anvier/", 'January', $sArg);
+$sArg = preg_replace('/[Ff]evrier/', 'February', $sArg);
+$sArg = preg_replace('/[Mm]ars/', 'March', $sArg);
+$sArg = preg_replace('/[Aa]vril/', 'April', $sArg);
+$sArg = preg_replace('/[Mm]ai/', 'May', $sArg);
+$sArg = preg_replace('/[Jj]uin/', 'June', $sArg);
+$sArg = preg_replace('/[Jj]uillet/', 'July', $sArg);
+$sArg = preg_replace('/[Aa]out/', 'August', $sArg);
+$sArg = preg_replace('/[Ss]eptembre/', 'September', $sArg);
+$sArg = preg_replace('/[Oo]ctobre/', 'October', $sArg);
+$sArg = preg_replace('/[Nn]ovembre/', 'November', $sArg);
+$sArg = preg_replace('/[Dd]ecembre/', 'December', $sArg);
 date_default_timezone_set("Europe/Paris");
-$time_string = strtotime($original);
-if (strlen($time_string) > 0)
-   echo strtotime($original) . "\n";
+$iReturn = strtotime($sArg);
+if ($iReturn > 0)
+	echo ($iReturn) . "\n";
 else
-	syntax_error();
+	exit1("Wrong Format");
 ?>
