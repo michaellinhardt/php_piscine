5a4096fa1dd6d2c89d83ede9883c2bca32c94e05
Michael LINHARDT <nestoyeur@gmail.com>
2016-06-18 20:32:54 +0200

    members.php list members

--- rush00/admin/member.php
+++ rush00/admin/member.php
@@ -4,5 +4,22 @@ $aMembers = $aData['members'];
 
 <div class="page_title">Liste des membres</div>
 <div class="page">
-	
+	<table class="table" id="box-table-a">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Login</th>
+				<th>Password</th>
+				<th>Mail</th>
+				<th>&nbsp;</th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
+			<?php
+			foreach( $aMembers as $iID => $aMember )
+				echo "<tr><td>".$iID."</td><td>".$aMember['login']."</td><td> *** </td><td>".$aMember['mail']."</td><td> 🗒 </td><td> ❌ </td></tr>";
+			?>
+		<tbody>
+		</tbody>
+	</table>
 </div>
