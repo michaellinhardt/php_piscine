502ff6bc7025713562b9ecc1523724ca7c81d9fb
Michael LINHARDT <nestoyeur@gmail.com>
2016-06-21 00:53:49 +0200

    d05 ex05

---
+++ d05/ex04/ex04.sql
@@ -0,0 +1,1 @@
+UPDATE `mlinhard`.`ft_table` SET `date_de_creation` = ADDDATE(`date_de_creation`, INTERVAL 20 YEAR) WHERE id > 5;
---
+++ d05/ex05/ex05.sql
@@ -0,0 +1,1 @@
+DELETE FROM `mlinhard`.`ft_table` WHERE id > 0 LIMIT 5;
