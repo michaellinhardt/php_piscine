c092cf6656f1ae83cd15e37c00a5dff07969e833
Michael LINHARDT <nestoyeur@gmail.com>
2016-06-21 00:50:49 +0200

    d05 ex02 and ex03

---
+++ d05/ex03/ex03.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mlinhard`.`ft_table` (`login`, `date_de_creation`, `groupe`)
+SELECT `nom`, `date_naissance`, "other" FROM fiche_personne WHERE CHAR_LENGTH(`nom`) < 9 AND INSTR(nom, 'a') > 0
+ORDER BY `nom` limit 10;
